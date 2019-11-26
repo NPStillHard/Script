62125d426102f83cb06f42307404df2a8e71d09a
NPStillHard <58206422+NPStillHard@users.noreply.github.com>
2019-11-25 23:09:54 -0800

    Create misc.sh

---
+++ misc.sh
@@ -0,0 +1,12 @@
+$ alias
+$ alias name='command line'
+$ unalias rm
+$ source .bash_profile
+$ source ~/.bashrc
+
+# Download 
+# youtube-dl
+$ youtube-dl -f bestaudio --extract-audio --audio-format mp3 --audio-quality 0 <Video-URL>
+
+# ffmpeg 
+$ ffmpeg -i input_file.flv output_file.mp3
